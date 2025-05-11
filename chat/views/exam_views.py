60c04a1b847ae207e71532dff742b410c19b5c29
Victor <victor.barrio@alumnos.upm.es>
2025-05-11 11:51:53 +0200

    Rename utils.js to evaluate_utils.js

--- chat/views/exam_views.py
+++ chat/views/exam_views.py
@@ -2,6 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from chat.models import Course, Model
+from chat.services.exam_service import process_exam_file #TODO no se puede quitar no se por que
 from chat.services.upload_file_service import handle_file_upload
 
 @login_required
