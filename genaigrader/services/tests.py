31e2ad0b6aa6bf320ae2f020ee6ae676642f6dd0
Victor <victor.barrio@alumnos.upm.es>
2025-05-12 19:41:17 +0200

    Added download Ollama models v1.1.6, Deleted type section from the table

--- genaigrader/services/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-from genaigrader.services.confidence_service import confidence_interval
-
-class ConfidenceServiceTest(TestCase):
-    def test_averages_two_values(self):
-        data = [5,10]
-        mean, cinf, csup = confidence_interval(data, 0.95)
-        self.assertEqual(mean, 7.5)
-        
